02189cc7033136cecffd5befe44e9de90b42a49f
Salim ABDELFETTAH <s.abd.fettah@gmail.com>
2018-06-05 19:46:55 +0200

    fix season save, refactoring and add batch script

--- main.py
+++ main.py
@@ -23,161 +23,164 @@ ARRAY_PLAYER_FANTASY_VALUES = ["INFLUENCE", "CREATIVITY", "THREAT", "ICT_INDEX"]
 ARRAY_PLAYER_DETAILS = ["GAME_WEEK", "OPP", "POINTS", "MINUTES_PLAYED", "GOALS_SCORED", "ASSISTS", "CLEAN_SHEETS", "GOALS_CONCEDED", "OWN_GOALS", "PINALTIES_SAVED", "PINALTIES_MISSED", "YELLOW_CARDS", "RED_CARDS", "SAVED", "BONUS", "BONUS_POINTS_SYSTEM", "INFLUENCE", "CREATIVITY", "THREAT", "ICT_INDEX", "NET_TRANSFERT", "SELECTED_BY", "PRICE"]
 ARRAY_PLAYER_OLD_SEASONS_DETAILS = ["SEASON", "POINTS", "MINUTES_PLAYED", "GOALS_SCORED", "ASSISTS", "CLEAN_SHEETS", "GOALS_CONCEDED", "OWN_GOALS", "PINALTIES_SAVED", "PINALTIES_MISSED", "YELLOW_CARDS", "RED_CARDS", "SAVED", "BONUS", "BONUS_POINTS_SYSTEM", "INFLUENCE", "CREATIVITY", "THREAT", "ICT_INDEX", "PRICE"]
 
-def getPrice(rawValue):
-	return rawValue[1:]
+def getPrice(raw_value):
+	return raw_value[1:]
 
-def getPoints(rawValue):
-	return rawValue.replace('pts', '')
+def getPoints(raw_value):
+	return raw_value.replace('pts', '')
 
-def getSelection(rawValue):
-	return rawValue.replace('%', '')
+def getSelection(raw_value):
+	return raw_value.replace('%', '')
 
-def extractPlayerDetails(driver, currentPlayer, playerLinkDetails):
-	if playerLinkDetails == None:
-		print 'not player link details button for', currentPlayer
+def extractPlayerDetails(driver, current_player, player_link_details):
+	if player_link_details == None:
+		print 'not player link details button for', current_player
 	else:
-		playerLinkDetails.click()
+		player_link_details.click()
 		# wait for header presence
-		dialogHeader = []
-		while len(dialogHeader) == 0:
-			dialogHeader = driver.find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_HEADER)
-			if len(dialogHeader) == 0:
+		dialog_header = []
+		while len(dialog_header) == 0:
+			dialog_header = driver.find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_HEADER)
+			if len(dialog_header) == 0:
 				time.sleep(0.5)
 		# looking for content
-		dialogScroll = []
-		while len(dialogScroll) == 0:
-			dialogScroll = driver.find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_SCROLL)
-			if len(dialogScroll) == 0:
+		dialog_scroll = []
+		while len(dialog_scroll) == 0:
+			dialog_scroll = driver.find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_SCROLL)
+			if len(dialog_scroll) == 0:
 				time.sleep(0.5)
 		# player data
-		fullName = dialogScroll[0].find_elements_by_class_name(SELECTOR_PLAYER_FULLNAME)
-		currentPlayer["FULLNAME"] = fullName[0].text
+		full_name = dialog_scroll[0].find_elements_by_class_name(SELECTOR_PLAYER_FULLNAME)
+		current_player["FULLNAME"] = full_name[0].text
 		# initialize dicts
-		seasons_details = currentPlayer["SEASONS_DETAILS"]
+		seasons_details = current_player["SEASONS_DETAILS"]
 		season_17_18_details = {}
 		# current season
 		# season game values
-		dataIndex = 0
-		for value in dialogScroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_GAME_VALUES):
-			if ARRAY_PLAYER_GAME_VALUES[dataIndex] == "GW38":
+		data_index = 0
+		for value in dialog_scroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_GAME_VALUES):
+			if ARRAY_PLAYER_GAME_VALUES[data_index] == "GW38":
 				pass
-			elif ARRAY_PLAYER_GAME_VALUES[dataIndex] == "POINTS":
+			elif ARRAY_PLAYER_GAME_VALUES[data_index] == "POINTS":
 				season_17_18_details["POINTS"] = getPoints(value.text)
-			elif ARRAY_PLAYER_GAME_VALUES[dataIndex] == "PRICE":
+			elif ARRAY_PLAYER_GAME_VALUES[data_index] == "PRICE":
 				season_17_18_details["PRICE"] = getPrice(value.text)
-			elif ARRAY_PLAYER_GAME_VALUES[dataIndex] == "SELECTION":
+			elif ARRAY_PLAYER_GAME_VALUES[data_index] == "SELECTION":
 				season_17_18_details["SELECTION"] = getSelection(value.text)
 			else:
-				season_17_18_details[ARRAY_PLAYER_GAME_VALUES[dataIndex]] = value.text
-			dataIndex = dataIndex + 1
+				season_17_18_details[ARRAY_PLAYER_GAME_VALUES[data_index]] = value.text
+			data_index = data_index + 1
 		# season fantasy values
-		dataIndex = 0
-		for value in dialogScroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_FANTASY_VALUES):
-			season_17_18_details[ARRAY_PLAYER_FANTASY_VALUES[dataIndex]] = value.text
-			dataIndex = dataIndex + 1
+		data_index = 0
+		for value in dialog_scroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_FANTASY_VALUES):
+			season_17_18_details[ARRAY_PLAYER_FANTASY_VALUES[data_index]] = value.text
+			data_index = data_index + 1
 		# tables
-		tableDetails = dialogScroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_TABLE)
+		table_details = dialog_scroll[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_TABLE)
 		# this season details
-		tableDetailsBody = tableDetails[0].find_elements_by_tag_name("tbody")
-		gameWeeks = {}
-		for playerGameInfo in tableDetailsBody[0].find_elements_by_tag_name("tr"):
-			dataIndex = 0
-			gameWeekInfo = {}
-			gameWeek = "?"
-			for gameInfo in playerGameInfo.find_elements_by_tag_name("td"):
-				if ARRAY_PLAYER_DETAILS[dataIndex] == "GAME_WEEK":
-					gameWeek = gameInfo.text
-					gameWeekInfo["GAME_WEEK"] = gameInfo.text
-				elif ARRAY_PLAYER_DETAILS[dataIndex] == "OPP":
+		table_details_body = table_details[0].find_elements_by_tag_name("tbody")
+		game_weeks = {}
+		for player_game_info in table_details_body[0].find_elements_by_tag_name("tr"):
+			data_index = 0
+			game_week_info = {}
+			game_week = "?"
+			for game_info in player_game_info.find_elements_by_tag_name("td"):
+				if ARRAY_PLAYER_DETAILS[data_index] == "GAME_WEEK":
+					game_week = game_info.text
+					game_week_info["GAME_WEEK"] = game_info.text
+				elif ARRAY_PLAYER_DETAILS[data_index] == "OPP":
 					pass
-				elif ARRAY_PLAYER_DETAILS[dataIndex] == "PRICE":
-					gameWeekInfo[ARRAY_PLAYER_DETAILS[dataIndex]] = getPrice(gameInfo.text)
+				elif ARRAY_PLAYER_DETAILS[data_index] == "PRICE":
+					game_week_info[ARRAY_PLAYER_DETAILS[data_index]] = getPrice(game_info.text)
 				else:
-					gameWeekInfo[ARRAY_PLAYER_DETAILS[dataIndex]] = gameInfo.text
-				dataIndex = dataIndex + 1
-			gameWeeks[gameWeek] = gameWeekInfo
-		season_17_18_details["GAME_WEEKS"] = gameWeeks
+					game_week_info[ARRAY_PLAYER_DETAILS[data_index]] = game_info.text
+				data_index = data_index + 1
+			game_weeks[game_week] = game_week_info
+		season_17_18_details["GAME_WEEKS"] = game_weeks
 		seasons_details["2017/18"] = season_17_18_details
 		# old seasons details
-		tableDetailsBody = tableDetails[1].find_elements_by_tag_name("tbody")
-		for oldSeason in tableDetailsBody[0].find_elements_by_tag_name("tr"):
-			dataIndex = 0
+		table_details_body = table_details[1].find_elements_by_tag_name("tbody")
+		for old_season in table_details_body[0].find_elements_by_tag_name("tr"):
+			data_index = 0
 			season = {}
-			seasonName = "?"
-			for seasonInfo in oldSeason.find_elements_by_tag_name("td"):
-				if ARRAY_PLAYER_OLD_SEASONS_DETAILS[dataIndex] == "SEASON":
-					seasonName = seasonInfo.text
-					season["SEASON"] = seasonInfo.text
-				elif ARRAY_PLAYER_OLD_SEASONS_DETAILS[dataIndex] == "PRICE":
-					season[ARRAY_PLAYER_OLD_SEASONS_DETAILS[dataIndex]] = getPrice(seasonInfo.text)
+			season_name = "?"
+			for season_info in old_season.find_elements_by_tag_name("td"):
+				if ARRAY_PLAYER_OLD_SEASONS_DETAILS[data_index] == "SEASON":
+					season_name = season_info.text
+					season["SEASON"] = season_info.text
+				elif ARRAY_PLAYER_OLD_SEASONS_DETAILS[data_index] == "PRICE":
+					season[ARRAY_PLAYER_OLD_SEASONS_DETAILS[data_index]] = getPrice(season_info.text)
 				else:
-					season[ARRAY_PLAYER_OLD_SEASONS_DETAILS[dataIndex]] = seasonInfo.text
-				dataIndex = dataIndex + 1
-			seasons_details[seasonName] = season
-		currentPlayer["SEASONS_DETAILS"] = seasons_details
-		closeButton = dialogHeader[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_CLOSE_BUTTON)
-		if len(closeButton) > 0:
-			closeButton[0].click()
+					season[ARRAY_PLAYER_OLD_SEASONS_DETAILS[data_index]] = season_info.text
+				data_index = data_index + 1
+			if season_name == '2017/18':
+				seasons_details["LAST_SEASON_GLOBAL"] = season
+			else:
+				seasons_details[season_name] = season
+		current_player["SEASONS_DETAILS"] = seasons_details
+		close_button = dialog_header[0].find_elements_by_class_name(SELECTOR_DIALOG_PLAYER_DETAILS_CLOSE_BUTTON)
+		if len(close_button) > 0:
+			close_button[0].click()
 
-def extractPlayerInfo(currentPlayerGolbalData):
-	currentPlayer = {}
-	dataIndex = 0
+def extractPlayerInfo(current_player_global_data):
+	current_player = {}
+	data_index = 0
 	name = "name"
-	playerLinkDetails = None
+	player_link_details = None
 	seasons_details = {}
 	last_season = {}
-	for playerInfo in currentPlayerGolbalData:
-		if ARRAY_PLAYER_DATA[dataIndex] == "STATUS":
+	for player_info in current_player_global_data:
+		if ARRAY_PLAYER_DATA[data_index] == "STATUS":
 			pass
-		elif ARRAY_PLAYER_DATA[dataIndex] == "NAME":
-			playerLinkDetails = playerInfo.find_element_by_class_name(SELECTOR_PLAYER_NAME)
-			name = playerLinkDetails.text
-			currentPlayer["NAME"] = name
-			currentPlayer["CLUB"] = playerInfo.find_element_by_class_name(SELECTOR_PLAYER_CLUB).text
-			currentPlayer["POSITION"] = playerInfo.find_element_by_class_name(SELECTOR_PLAYER_POSITION).text
-		elif ARRAY_PLAYER_DATA[dataIndex] == "PRICE":
-			last_season["PRICE"] = getPrice(playerInfo.text)
-		elif ARRAY_PLAYER_DATA[dataIndex] == "SELECTION":
-			last_season["SELECTION"] = getSelection(playerInfo.text)
+		elif ARRAY_PLAYER_DATA[data_index] == "NAME":
+			player_link_details = player_info.find_element_by_class_name(SELECTOR_PLAYER_NAME)
+			name = player_link_details.text
+			current_player["NAME"] = name
+			current_player["CLUB"] = player_info.find_element_by_class_name(SELECTOR_PLAYER_CLUB).text
+			current_player["POSITION"] = player_info.find_element_by_class_name(SELECTOR_PLAYER_POSITION).text
+		elif ARRAY_PLAYER_DATA[data_index] == "PRICE":
+			last_season["PRICE"] = getPrice(player_info.text)
+		elif ARRAY_PLAYER_DATA[data_index] == "SELECTION":
+			last_season["SELECTION"] = getSelection(player_info.text)
 		else:
-			last_season[ARRAY_PLAYER_DATA[dataIndex]] = playerInfo.text
-		dataIndex = dataIndex + 1
+			last_season[ARRAY_PLAYER_DATA[data_index]] = player_info.text
+		data_index = data_index + 1
 	seasons_details["LAST_SEASON"] = last_season
-	currentPlayer["SEASONS_DETAILS"] = seasons_details
-	return name, currentPlayer, playerLinkDetails
+	current_player["SEASONS_DETAILS"] = seasons_details
+	return name, current_player, player_link_details
 
-def getPlayerInfo(driver, playersInCurrentPage, players, playersLinks):
-	for player in playersInCurrentPage:
+def getPlayerInfo(driver, players_in_current_page, players, players_links):
+	for player in players_in_current_page:
 		print 'Processing a player ...'
-		currentPlayerGolbalData = player.find_elements_by_tag_name("td")
-		name, currentPlayer, playerLinkDetails = extractPlayerInfo(currentPlayerGolbalData)
-		extractPlayerDetails(driver, currentPlayer, playerLinkDetails)
-		filePlayer = open('dumps/player_' + currentPlayer["FULLNAME"].replace(' ', '_') + '.json', 'w')
-		filePlayer.write(json.dumps(currentPlayer))
-		filePlayer.close()
-		players[name] = currentPlayer
-		print 'Player', currentPlayer["FULLNAME"], 'processed'
+		current_player_global_data = player.find_elements_by_tag_name("td")
+		name, current_player, player_link_details = extractPlayerInfo(current_player_global_data)
+		extractPlayerDetails(driver, current_player, player_link_details)
+		file_player = open('dumps/player_' + current_player["FULLNAME"].replace(' ', '_') + '.json', 'w')
+		file_player.write(json.dumps(current_player))
+		file_player.close()
+		players[name] = current_player
+		print 'Player', current_player["FULLNAME"], 'processed'
 
 
 # initialization
 print 'Program start'
 driver = webdriver.Chrome()
 players = {}
-clubNumber = 1
+club_number = 1
 # look for table that contains player data
-for clubNumber in range(20):
-	print 'Processing club', clubNumber + 1, '...'
-	driver.get(FPL_LINK + '/te_' + str(clubNumber + 1))
+for club_number in range(20):
+	print 'Processing club', club_number + 1, '...'
+	driver.get(FPL_LINK + '/te_' + str(club_number + 1))
 	table = []
 	while len(table) == 0:
 		table = driver.find_elements_by_class_name(SELECTOR_TABLE_PLAYERS)
 		if len(table) == 0:
 			time.sleep(0.5)
-	tableBody = table[0].find_elements_by_tag_name("tbody")
-	playersInCurrentPage = tableBody[0].find_elements_by_tag_name("tr")
-	playersLinks = {}
-	getPlayerInfo(driver, playersInCurrentPage, players, playersLinks)
-filePlayers = open('dumps/players_dump.json', 'w')
-filePlayers.write(json.dumps(players))
-filePlayers.close()
+	table_body = table[0].find_elements_by_tag_name("tbody")
+	players_in_current_page = table_body[0].find_elements_by_tag_name("tr")
+	players_links = {}
+	getPlayerInfo(driver, players_in_current_page, players, players_links)
+file_players = open('dumps/players_dump.json', 'w')
+file_players.write(json.dumps(players))
+file_players.close()
 print 'Program end'
